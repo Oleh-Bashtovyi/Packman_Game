2642c85aa70be8c85269746c85b98dd94ff493d9
Oleh-Bashtovyi <olehbashtovyi@gmail.com>
2024-02-15 17:36:48 +0200

    Implemented base position object.

---
+++ Position.py
@@ -0,0 +1,24 @@
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return 2
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Index out of range")
+
+    def __add__(self, other):
+        if isinstance(other, (Position, list, tuple)):
+            if len(other) >= 2:
+                return Position(self.x + other[0], self.y + other[1])
+            else:
+                raise ValueError("Input list or tuple must have exactly 2 elements")
+        else:
+            raise TypeError("Unsupported operand type")
