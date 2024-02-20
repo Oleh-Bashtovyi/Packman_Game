7d3f38e554e0632b1da08ce7c68191d33dac2f94
sashaboiko7 <oleksandraboiko777@gmail.com>
2024-02-20 13:18:13 +0200

    create class MazeController for ghosts moving

--- MazeController.py
+++ MazeController.py
@@ -20,18 +20,47 @@ class MazeNode:
 
 
 
-# TODO
-# FINISH MAZE CONTROLLER
-# - OLEH says:
-# - контролер повинен зчитувати лабіринт і перетворювати кожну клітину в ньому на Node.
-# - Node має сусідів, тобто клітини на які ми можемо перейти з цього нода.
-#   Це потрібно для привидів, щоб вони знали, на які позиції можуть перейти.
-# - Ваше завдання написати алгоритм зчитування, створення і заповнення сусідів нодів.
-# - Думаю цей контролер має бути десь в gameState.
-#   В gameState хай зберігається в змінній self.mazeController
 class MazeController:
-    def __init__(self):
-        self.something = 1
+     def __init__(self):
+        self.numpy_maze = []
+        self.cookie_spaces = []
+        self.powerup_spaces = []
+        self.reachable_spaces = []
+        self.ghost_spawns = []
+        self.size = (0, 0)
+        self.convert_maze_to_numpy()
+        self.p = Pathfinder(self.numpy_maze)
+        self.nodes: Dict[Tuple[int, int], MazeNode] = {}
 
-    def get_node_at_position(self, position: Position):
-        pass
+
+     def add_node(self, position: Position):
+        if position not in self.nodes:
+            self.nodes[position] = MazeNode(position.x, position.y)
+
+     def add_edge(self, position1: Position, position2: Position, direction: Direction):
+        node1 = self.nodes.get(position1)
+        node2 = self.nodes.get(position2)
+        if node1 and node2:
+            node1.add_neighbour(direction, node2)
+            # Припускаючи, що лабіринт без напрямку, додаємо зв'язок у обидві сторони.
+            opposite_direction = self.opposite(direction)
+            node2.add_neighbour(opposite_direction, node1)
+
+
+     def read_maze(self, maze: List[str]):
+        for y, row in maze:
+            for x, cell in row:
+                position = Position(x, y)
+                self.add_node(position)
+                # Перевірка сусідів
+                if (x > 0 and row[x-1] != 'X'):
+                    self.add_edge(position, Position(x - 1, y), Direction.LEFT)
+                if (x < len(row) - 1 and row[x+1] != 'X'):
+                    self.add_edge(position, Position(x + 1, y), Direction.RIGHT)
+                if (y > 0 and maze[y-1, x] != 'X'): 
+                    self.add_edge(position, Position(x, y - 1), Direction.UP)
+                if (y < len(maze) - 1 and maze[y+1, x] != 'X'):
+                    self.add_edge(position, Position(x, y + 1), Direction.DOWN)
+
+     def get_node_at_position(self, position: Position) -> MazeNode:
+       return self.nodes.get(position)
