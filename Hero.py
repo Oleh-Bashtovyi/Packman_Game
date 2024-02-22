0ab02a193f6790422ff82df43fcc9e49cea34bc3
sashaboiko7 <oleksandraboiko777@gmail.com>
2024-02-22 21:17:18 +0200

    game finished, no start debuging

--- Hero.py
+++ Hero.py
@@ -10,8 +10,8 @@ class Hero(Entity):
                  spawn_position: Position):
         super().__init__(game_state, spawn_position, PACMAN_SIZE, (255, 255, 0), False)
         self.last_non_colliding_position = (0, 0)
-        self.open = pygame.image.load("images/paku.png")
-        self.closed = pygame.image.load("images/man.png")
+        self.open = pygame.image.load("images/pacman_open.png")
+        self.closed = pygame.image.load("images/pacman_close.png")
         self.image = self.open
         self.mouth_open = True
 
