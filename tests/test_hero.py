d972a56992be680d194fd952717fcf5f9f2d4c06
sashaboiko7 <oleksandraboiko777@gmail.com>
2024-05-15 13:57:37 +0300

    add test_draw

---
+++ tests/test_hero.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+from Position import Position
+from Hero import Hero 
+from GameState import GameState
+import pytest
+
+@pytest.fixture
+def game_state():
+    tile_size = 10
+    maze_height = 20  
+    maze_width = 30
+    return GameState(tile_size, maze_height, maze_width)
+
+@pytest.fixture
+def hero(game_state):
+    hero = Hero(game_state, Position(0, 0))
+    hero._renderer = MagicMock()
+    return hero
+
+def test_draw(hero):
+    initial_image = hero._entity_image
+    hero.draw()
+    assert hero._entity_image != initial_image
+
+
+
+
+
+
