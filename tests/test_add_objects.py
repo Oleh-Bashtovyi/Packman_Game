8fd7eaa66b2c9b0af02e8dbad8b5bd91944ca96f
anamor76y <anna.moroz@knu.ua>
2024-05-15 14:27:15 +0300

    add file test_add_objects

---
+++ tests/test_add_objects.py
@@ -0,0 +1,22 @@
+import pytest
+from GameObjects import *
+from Hero import *
+from Ghosts import *
+from GameState import *
+from GameObjects import GameObject, Position
+
+@pytest.fixture
+def game_state():
+    return GameState(tile_size=32, maze_height=20, maze_width=20)
+
+@pytest.fixture
+def game_state():
+    from GameState import GameState  
+    return GameState(32, 10, 10)
+
+def test_add_game_object(game_state):
+    obj_size = 10  
+    obj = GameObject(game_state, Position(1, 1), obj_size)  
+    game_state.add_game_object(obj)
+    assert obj in game_state.get_game_objects()
+
