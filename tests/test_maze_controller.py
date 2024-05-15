8f4676410fffac326ec572e7a70401cb590d4525
sashaboiko7 <oleksandraboiko777@gmail.com>
2024-05-15 10:09:37 +0300

    add test_get_walkable_positions

--- tests/test_maze_controller.py
+++ tests/test_maze_controller.py
@@ -13,3 +13,14 @@ def maze_controller_with_nodes():
     controller.add_node(Position(0, 1))
     return controller
 
+def test_get_walkable_positions():
+    node1 = MazeNode(0, 0)
+    node2 = MazeNode(1, 0)
+    node3 = MazeNode(0, 1)
+    node1.add_neighbour(Direction.RIGHT, node2)
+    node1.add_neighbour(Direction.DOWN, node3)
+
+    expected_positions = [(Direction.RIGHT, Position(1, 0)), (Direction.DOWN, Position(0, 1))]
+    assert node1.get_walkable_positions() == expected_positions
+
+
